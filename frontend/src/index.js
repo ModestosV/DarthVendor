7e682360c4ad2fda26a2c3c8f838a5683f03b39d
xTEddie <edwardtranv1@gmail.com>
2017-09-15 20:04:19 -0400

    Add basic ReactJS setup

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home/';
+
+
+ReactDOM.render(
+    <Home />,
+    document.getElementById('root')
+)
