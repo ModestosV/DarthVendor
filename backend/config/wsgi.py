156e8d77549b92dde8ba9ad94d220ad46232ffc9
xTEddie <edwardtranv1@gmail.com>
2017-09-15 22:06:18 -0400

    Update wsgi.py

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,7 +1,8 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from environment import SETTINGS_MODULE
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "darthvendor.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE.strip('config.'))
 
 application = get_wsgi_application()
