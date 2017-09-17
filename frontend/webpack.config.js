a422ce3ac484aed397cdfa3066e9b9ee1c1d45ca
xTEddie <edwardtranv1@gmail.com>
2017-09-16 23:21:16 -0400

    Update API_HOST

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 let API_HOST = '';
-if (process.env.NODE_ENV === 'production') API_HOST = '165.227.44.65:8000';
+if (process.env.NODE_ENV === 'production') API_HOST = 'api.darthvendor.me';
 
 module.exports = {
     entry: {
