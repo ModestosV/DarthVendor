09bad6d5d9a67ab5096d3d121ac2772d830c2a8d
xTEddie <edwardtranv1@gmail.com>
2017-09-20 03:08:28 -0400

    Add Django command to populate admin table with an admin

--- backend/apps/v1/accounts/management/commands/populate_admin_table.py
+++ backend/apps/v1/accounts/management/commands/populate_admin_table.py
@@ -1,27 +1,27 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from sqlite3 import dbapi2 as Database, OperationalError
+from sqlite3 import dbapi2 as Database
 
 
 class Command(BaseCommand):
-    help = 'Create admin table'
+    help = 'Populate admin table'
 
     def handle(self, *args, **options):
 
         connection = Database.connect(settings.DATABASES['default']['NAME'])
         cursor = connection.cursor()
         query = """
-            CREATE TABLE administrator (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                username varchar(255) UNIQUE,
-                password varchar(255)
+            INSERT INTO administrator (username, password)
+            VALUES (                
+                'foobar',
+                'D4rthV3nD0r'
             );
         """
 
         try:
             cursor.execute(query)
         except Exception as error: 
-            print(error)  
+            print(error)    
 
         connection.commit()
         connection.close()
--- scripts/populate_db.sh
+++ scripts/populate_db.sh
@@ -1,4 +1,6 @@
 echo "Create Admin Table"
 python3 manage.py create_admin_table
+echo "Populate Admin Table"
+python3 manage.py populate_admin_table
 
 echo "Done"
