d268618a53855fca70643ae91113a59e536049a3
xTEddie <edwardtranv1@gmail.com>
2017-09-20 04:28:01 -0400

    Add API to get individual admin detail

--- backend/apps/v1/accounts/urls.py
+++ backend/apps/v1/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url
-from .views.admin import AdminView
+from .views.admin import AdminView, AdminDetail 
 
 
 urlpatterns = [
-    url(r'^admins$', AdminView.as_view())
+    url(r'^admins$', AdminView.as_view()),
+    url(r'^admins/(?P<admin_id>[\d]+)$', AdminDetail.as_view()),
 ]
--- backend/apps/v1/accounts/views/admin.py
+++ backend/apps/v1/accounts/views/admin.py
@@ -40,3 +40,35 @@ class AdminView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class AdminDetail(APIView):
+    """
+        API: admins/{admin_id}
+    """
+
+    def get(self, request, admin_id):
+        """
+            View individual admin.
+        """
+
+        connection = Database.connect(settings.DATABASES['default']['NAME'])
+        connection.row_factory = dict_factory
+        cursor = connection.cursor()
+        query = """
+            SELECT *
+            FROM administrator
+            WHERE id={}
+        """.format(admin_id)
+
+        try:
+            cursor.execute(query)            
+            serializer = AdminSerializer(data=cursor.fetchall(), many=True)
+
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error: 
+            print(error) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
