ea4ed6b13b545333c822d0315c01dbac7a7c8d0a
xTEddie <edwardtranv1@gmail.com>
2017-09-20 04:59:16 -0400

    Add basic view for login. Missing token.

--- backend/apps/v1/accounts/urls.py
+++ backend/apps/v1/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.conf import settings
 from django.conf.urls import url
+from .views.login import LoginView
 from .views.admin import AdminView, AdminDetail 
 
 
 urlpatterns = [
+
+    # Login / Logout
+    url(r'^login$', LoginView.as_view()),
+
     url(r'^admins$', AdminView.as_view()),
     url(r'^admins/(?P<admin_id>[\d]+)$', AdminDetail.as_view()),
 ]
---
+++ backend/apps/v1/accounts/views/login.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.contrib.auth.hashers import check_password
+from django.db import connection
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from sqlite3 import dbapi2 as Database
+from utils.database import dict_factory
+from backend.apps.v1.accounts.serializers.admin import AdminSerializer
+
+
+class LoginView(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request):
+        """
+            Get user data and API token.
+        """
+        
+        connection = Database.connect(settings.DATABASES['default']['NAME'])
+        connection.row_factory = dict_factory
+        cursor = connection.cursor()
+        query = """
+            SELECT *
+            FROM administrator
+            WHERE username='{}'
+        """.format(request.data.get('username'))
+
+        try:
+            cursor.execute(query)    
+            user = cursor.fetchone()
+            serializer = AdminSerializer(data=user)
+
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            if not check_password(request.data.get('password'), user["password"]):
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as error: 
+            print(error) 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
