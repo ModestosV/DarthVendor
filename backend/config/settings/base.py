2a522cf731982f419e7bb466c62de8875c76ec2c
xTEddie <edwardtranv1@gmail.com>
2017-09-20 20:42:53 -0400

    Add CORS middleware

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -28,6 +28,7 @@ THIRD_PARTY_APPS = [
 INSTALLED_APPS = DJANGO_APPS +  THIRD_PARTY_APPS
 
 MIDDLEWARE_CLASSES = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
