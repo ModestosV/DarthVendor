9ad6db07c039789a9d54398e38d949fbf1f19535
chriskfwoo <chriskfwoo@gmail.com>
2017-09-28 17:35:38 -0400

    remove semantics stylesheet

--- frontend/src/components/Merchant/Navigation/index.js
+++ frontend/src/components/Merchant/Navigation/index.js
@@ -1,86 +1,86 @@
-// import axios from 'axios';
-// import React, {Component} from 'react';
-// import {Link, withRouter} from 'react-router-dom';
-// import settings from '../../config/settings';
-// import './Navigation.scss';
+import axios from 'axios';
+import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import settings from '../../config/settings';
+import './Navigation.scss';
 
 
-// class Navigation extends Component {
+class Navigation extends Component {
 
-//     handleLogOutButton() {
-//         const {history} = this.props;
-//         let headers = {
-//             'authorization': JSON.parse(localStorage.activeUser)['token'] 
-//         };        
-//         let config = {
-//             'headers': headers
-//         };
+    handleLogOutButton() {
+        const {history} = this.props;
+        let headers = {
+            'authorization': JSON.parse(localStorage.activeUser)['token'] 
+        };        
+        let config = {
+            'headers': headers
+        };
         
-//         axios.get(`${settings.API_ROOT}/logout`, config)
-//             .then(response => {
-//                 console.log(response);
-//                 localStorage.setItem('activeUser', '');
-//                 history.push('/login');
-//             })
-//             .catch(error => {
-//                 console.log(error);                
-//             })
-//     }
+        axios.get(`${settings.API_ROOT}/logout`, config)
+            .then(response => {
+                console.log(response);
+                localStorage.setItem('activeUser', '');
+                history.push('/login');
+            })
+            .catch(error => {
+                console.log(error);                
+            })
+    }
 
-//     renderToggleButton() {
-//         return (
-//             <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>   
-//         )
-//     }
+    renderToggleButton() {
+        return (
+            <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>   
+        )
+    }
 
-//     renderLogOutButton() {
-//         return (
-//             <ul className="navbar-nav">
-//                 <li className="nav-item">
-//                     <button 
-//                         className="btn btn-danger"
-//                         onClick={() => this.handleLogOutButton()}
-//                     >
-//                         Log Out
-//                         {" "} 
-//                         <i className="fa fa-sign-out" aria-hidden="true"></i>                                                
-//                     </button>
-//                 </li>        
-//             </ul>
-//         )
-//     }
+    renderLogOutButton() {
+        return (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <button 
+                        className="btn btn-danger"
+                        onClick={() => this.handleLogOutButton()}
+                    >
+                        Log Out
+                        {" "} 
+                        <i className="fa fa-sign-out" aria-hidden="true"></i>                                                
+                    </button>
+                </li>        
+            </ul>
+        )
+    }
 
-//     render() {
-//         return (    
-//             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//                 <a className="navbar-brand" href="#">DarthVendor </a>
+    render() {
+        return (    
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">DarthVendor </a>
 
-//                 {this.renderToggleButton()}
+                {this.renderToggleButton()}
                 
-//                 <div className="navbar-collapse collapse" id="myNavbar">
-//                     <ul className="navbar-nav mr-auto">
-//                         <li className="nav-item active">
-//                             <Link className="nav-link" to={`/`}>
-//                                 <i className="fa fa-list-alt" aria-hidden="true"></i>
-//                                 {" "} Home 
-//                                 <span className="sr-only">(current)</span>
-//                             </Link>
-//                         </li>
-//                         <li className="nav-item">
-//                             <Link className="nav-link" to={`/catalog`}>
-//                                 <i className="fa fa-folder" aria-hidden="true"></i>                            
-//                                 {" "} Catalog
-//                             </Link>
-//                         </li>                                                
-//                     </ul>   
+                <div className="navbar-collapse collapse" id="myNavbar">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to={`/`}>
+                                <i className="fa fa-list-alt" aria-hidden="true"></i>
+                                {" "} Home 
+                                <span className="sr-only">(current)</span>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={`/catalog`}>
+                                <i className="fa fa-folder" aria-hidden="true"></i>                            
+                                {" "} Catalog
+                            </Link>
+                        </li>                                                
+                    </ul>   
 
-//                     {this.renderLogOutButton()}
-//                 </div>
-//             </nav>         
-//         )
-//     }
-// }
+                    {this.renderLogOutButton()}
+                </div>
+            </nav>         
+        )
+    }
+}
 
-// export default withRouter(Navigation);
+export default withRouter(Navigation);
