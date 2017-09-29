15eaa995926b2c8483bf9b4805c87b6d191b48f9
xTEddie <edwardtranv1@gmail.com>
2017-09-29 18:03:39 -0400

    Move utils folder to backend

--- backend/apps/v1/accounts/views/admin.py
+++ backend/apps/v1/accounts/views/admin.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from utils.database import Database
+from backend.utils.database import Database
 from backend.apps.v1.accounts.serializers.admin import AdminSerializer
 
 
--- backend/apps/v1/accounts/views/logout.py
+++ backend/apps/v1/accounts/views/logout.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from utils.database import Database
+from backend.utils.database import Database
 
 
 class LogoutView(APIView):
