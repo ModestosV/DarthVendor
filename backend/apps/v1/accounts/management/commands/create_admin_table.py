97863ffe92030d8927fd945b47107e70f1e6e4f3
xTEddie <edwardtranv1@gmail.com>
2017-09-29 21:34:30 -0400

    Update administrator DB schema

--- backend/apps/v1/accounts/management/commands/create_admin_table.py
+++ backend/apps/v1/accounts/management/commands/create_admin_table.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from sqlite3 import dbapi2 as Database
 
 
@@ -14,14 +14,19 @@ class Command(BaseCommand):
             CREATE TABLE administrator (
                 id integer PRIMARY KEY AUTOINCREMENT,
                 username varchar(255) UNIQUE,
+                firstname varchar(255) DEFAULT '',
+                lastname varchar(255) DEFAULT '',
+                email varchar(255) DEFAULT '',
+                address varchar(255) DEFAULT '',
+                phone varchar(255) DEFAULT '',
                 password varchar(255)
             );
         """
 
         try:
             cursor.execute(query)
-        except Exception as error: 
-            print(error)  
+        except Exception as error:
+            print(error)
 
         connection.commit()
         connection.close()
--- backend/apps/v1/accounts/management/commands/populate_admin_table.py
+++ backend/apps/v1/accounts/management/commands/populate_admin_table.py
@@ -1,13 +1,15 @@
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from sqlite3 import dbapi2 as Database
-from django.contrib.auth.hashers import (
-    check_password, is_password_usable, make_password,
-)
+from django.contrib.auth.hashers import make_password
 
 
+FIRSTNAME = 'foo'
+LASTNAME = 'bar'
 USERNAME = 'foobar'
+EMAIL = 'foobar@darthvendor.com'
 PASSWORD = 'D4rthV3nD0r'
+
 
 class Command(BaseCommand):
     help = 'Populate admin table'
@@ -17,17 +19,14 @@ class Command(BaseCommand):
         connection = Database.connect(settings.DATABASES['default']['NAME'])
         cursor = connection.cursor()
         query = """
-            INSERT INTO administrator (username, password)
-            VALUES (                
-                '{}',
-                '{}'
-            );
-        """.format(USERNAME, make_password(PASSWORD))
+            INSERT INTO administrator (username, firstname, lastname, email, password)
+            VALUES ('{}', '{}', '{}', '{}', '{}');
+        """.format(USERNAME, FIRSTNAME, LASTNAME, EMAIL, make_password(PASSWORD))
 
         try:
             cursor.execute(query)
-        except Exception as error: 
-            print(error)    
+        except Exception as error:
+            print(error)
 
         connection.commit()
         connection.close()
