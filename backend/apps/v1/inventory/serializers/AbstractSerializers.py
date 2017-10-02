5de1c7bd384505d3b374cccbac560bd104052bcc
xTEddie <edwardtranv1@gmail.com>
2017-10-02 05:33:48 -0400

    Add type and quantity serializer fields

--- backend/apps/v1/inventory/serializers/AbstractSerializers.py
+++ backend/apps/v1/inventory/serializers/AbstractSerializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from backend.utils.database import Database
 
 
 class ItemSpecificationSerializer(serializers.Serializer):
@@ -8,6 +9,40 @@ class ItemSpecificationSerializer(serializers.Serializer):
     price = serializers.DecimalField(max_digits=20, decimal_places=2)
     priceFormat = serializers.CharField()
     brandName = serializers.CharField()
+    type = serializers.SerializerMethodField()
+    quantity = serializers.SerializerMethodField()
+
+    def get_type(self, item):
+        with Database() as cursor:            
+            query = """
+                SELECT *
+                FROM item
+                WHERE modelNumber='{}'
+            """.format(item.modelNumber)
+
+            try:
+                cursor.execute(query)
+                item = cursor.fetchone()                
+                return item["type"].upper()
+            except Exception as error:
+                print(error)
+                return None
+
+    def get_quantity(self, item):
+        with Database() as cursor:            
+            query = """
+                SELECT *
+                FROM item
+                WHERE modelNumber='{}'
+            """.format(item.modelNumber)
+
+            try:
+                cursor.execute(query)
+                item = cursor.fetchone()                
+                return item["quantity"]
+            except Exception as error:
+                print(error)
+                return None
 
 
 class AbstractComputerSerializer(ItemSpecificationSerializer):
