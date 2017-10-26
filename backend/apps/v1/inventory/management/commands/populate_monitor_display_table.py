434a6e2f6059067399c0cb5ff3fcbdbca844c987
mikebasdeo <mikebasdeo@gmail.com>
2017-10-26 19:06:45 -0400

    Update populate_monitor_display_table.py

--- backend/apps/v1/inventory/management/commands/populate_monitor_display_table.py
+++ backend/apps/v1/inventory/management/commands/populate_monitor_display_table.py
@@ -4,33 +4,45 @@ from backend.utils.database import Database
 
 monitors = [
     dict(
+        modelNumber='x123131',
+        name='Gaming Monitor',
         quantity=5,
         weight=10.0,
         weightFormat='lbs',
         price=9.99,
         priceFormat='CAD',
         brandName='SONY',
-        name='Gaming Monitor',
         type='monitor',
-        modelNumber='x123131',
         size=30.5,
         sizeFormat='inch'
     ),
     dict(
-        quantity=10,
-        weight=5.0,
+        modelNumber='v123131',
+        name='Regular Monitor',
+        quantity=5,
+        weight=10.0,
         weightFormat='lbs',
-        price=19.99,
+        price=9.99,
         priceFormat='CAD',
-        brandName='SAMSUNG',
-        name='Curve Display',
+        brandName='SORNY',
         type='monitor',
-        modelNumber='x80942380942',
-        size=50.5,
+        size=30.5,
         sizeFormat='inch'
     )
 ]
 
+monitorDisplayIDs = [
+	dict(
+	    serialNum='g569s79874',
+	    modelNum='x123131',
+	    isLocked=0
+	),
+	dict(
+	    serialNum='TWERTERTERRK',
+	    modelNum='v123131',
+	    isLocked=0
+	)
+]
 
 class Command(BaseCommand):
     help = 'Populate monitor display table'
@@ -41,23 +53,24 @@ class Command(BaseCommand):
 
             for monitor in monitors:
                 query = """
-                    INSERT INTO item (quantity, name, weight, weightFormat, price,
-                        priceFormat, brandName, type, modelNumber)
-                    VALUES ({quantity}, '{name}', {weight}, '{weightFormat}', 
-                        {price}, '{priceFormat}', '{brandName}', '{type}', '{modelNumber}');
+                    INSERT INTO monitorDisplay (modelNumber, quantity, name, weight, weightFormat, price, priceFormat, 
+                                                brandName, type, size, sizeFormat)
+                    VALUES ('{modelNumber}', {quantity}, '{name}', {weight}, '{weightFormat}', {price}, '{priceFormat}', 
+                             '{brandName}', '{type}', {size}, '{sizeFormat}');
                 """.format(**monitor)
 
                 try:
                     cursor.execute(query)
                 except Exception as error:
                     print(error)
-
-                query = """
-                    INSERT INTO monitorDisplay (modelNumber, size, sizeFormat)
-                    VALUES ('{modelNumber}', {size}, '{sizeFormat}');
-                """.format(**monitor)
-
-                try:
+                    
+            for monitorDisplayID in monitorDisplayIDs:
+                 query = """
+		            INSERT INTO monitorDisplayID (serialNum, modelNum, isLocked)
+		            VALUES ('{serialNum}', '{modelNum}', {isLocked});
+		         """.format(**monitorDisplayID)
+				
+	             try:
                     cursor.execute(query)
                 except Exception as error:
-                    print(error)
+                    print(error)                    
