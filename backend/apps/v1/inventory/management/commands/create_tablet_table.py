f30922ae0fed3043f9d6752c03580740407e601d
mikebasdeo <mikebasdeo@gmail.com>
2017-10-26 18:43:54 -0400

    Update create_tablet_table.py

--- backend/apps/v1/inventory/management/commands/create_tablet_table.py
+++ backend/apps/v1/inventory/management/commands/create_tablet_table.py
@@ -12,10 +12,17 @@ class Command(BaseCommand):
             query = """
                 CREATE TABLE tablet (
                     modelNumber varchar(255) NOT NULL,
-                    ramSize integer NOT NULL,
+                    quantity integer(11) NOT NULL,
+                    name varchar(255) NOT NULL,
+                    weight double NOT NULL,
+                    weightFormat varchar(255) NOT NULL,
+                    price double NOT NULL,
+                    priceFormat varchar(255) NOT NULL,
+                    type varchar(255) NOT NULL,
+                    ramSize integer(11) NOT NULL,
                     ramFormat varchar(255) NOT NULL,
                     processorType varchar(255) NOT NULL,
-                    numCores integer NOT NULL,
+                    numCores integer(11) NOT NULL,
                     hardDriveSize double NOT NULL,
                     hardDriveFormat varchar(255) NOT NULL,
                     os varchar(255) NOT NULL,
@@ -27,12 +34,25 @@ class Command(BaseCommand):
                     cameraInfo varchar(255) NOT NULL,
                     size double NOT NULL,
                     sizeFormat varchar(255) NOT NULL,
-                    PRIMARY KEY (modelNumber),
-                    FOREIGN KEY (modelNumber) REFERENCES item (modelNumber)
+                    PRIMARY KEY (modelNumber)
                 );
             """
 
             try:
                 cursor.execute(query)
             except Exception as error: 
+                print(error)
+                
+            query = """
+                CREATE TABLE tabletID (
+                    serialNum varchar(255) NOT NULL,
+                    modelNum varchar(255) NOT NULL,
+                    isLocked tinyint(1) DEFAULT 0,
+                    PRIMARY KEY (serialNum),
+                    FOREIGN KEY (modelNum) REFERENCES tablet (modelNumber)
+                );
+            """
+            try:
+                cursor.execute(query)
+            except Exception as error:
                 print(error)
