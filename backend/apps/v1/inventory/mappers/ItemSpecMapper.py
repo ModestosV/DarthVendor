57a2c2e7066653817424b1e6e63635e6efaef1dd
Frederic Daigle <Freddydaigle@gmail.com>
2017-10-27 14:55:11 -0400

    fixed stuff, started ItemIDMapper

--- backend/apps/v1/inventory/mappers/ItemSpecMapper.py
+++ backend/apps/v1/inventory/mappers/ItemSpecMapper.py
@@ -6,24 +6,25 @@ from backend.apps.v1.inventory.models.Size import Size
 from backend.apps.v1.inventory.models.Tablet import Tablet
 from backend.utils.database import Database
 
-class ItemSpecMapper.py:
-    
+
+class ItemSpecMapper:
+
     @staticmethod
     def insert(itemspec):
 
-    
+
     @staticmethod
     def update(itemspec):
-    
+
     @staticmethod
     def lock(type, uow):
         if(itemspec is Desktop):
         elif(itemspec is Laptop):
         elif(itemspec is MonitorDisplay):
         elif(itemspec is Tablet):
-    
+
     @staticmethod
     def unlock(type):
-    
+
     @staticmethod
     def find(num):
