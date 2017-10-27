61982421233394ee3bca65c871620e10baa01718
Andres Nunez <a.nunez.zegarra@gmail.com>
2017-10-26 21:03:36 -0400

    started ItemSpecMapper

---
+++ backend/apps/v1/inventory/mappers/ItemSpecMapper.py
@@ -0,0 +1,21 @@
+from backend.utils.database import Database 
+from backend.apps.v1.inventory.models.Desktop import Desktop
+from backend.apps.v1.inventory.models.Dimension import Dimension
+from backend.apps.v1.inventory.models.Size import Size
+
+class ItemSpecMapper.py:
+    
+    @staticmethod
+    def insert(itemspec):
+    
+    @staticmethod
+    def update(itemspec):
+    
+    @staticmethod
+    def lock(type, uow):
+    
+    @staticmethod
+    def unlock(type):
+    
+    @staticmethod
+    def find(num):
