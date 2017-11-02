5647cb5379c65d2e1222420ff3f0e03b77a49e1d
Frederic Daigle <Freddydaigle@gmail.com>
2017-11-01 20:27:01 -0400

    modified serializers, figuring out testing

--- backend/apps/v1/inventory/serializers/LaptopSerializer.py
+++ backend/apps/v1/inventory/serializers/LaptopSerializer.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from backend.apps.v1.inventory.serializers.AbstractSerializers import AbstractComputerSerializer
-from backend.apps.v1.inventory.serializers.SizeSerializer import SizeSerializer
 
 
 class LaptopSerializer(AbstractComputerSerializer):
--- backend/apps/v1/inventory/views/item.py
+++ backend/apps/v1/inventory/views/item.py
@@ -7,14 +7,42 @@ from backend.apps.v1.inventory.models.Desktop import Desktop
 from backend.apps.v1.inventory.models.MonitorDisplay import MonitorDisplay
 from backend.apps.v1.inventory.models.Laptop import Laptop
 from backend.apps.v1.inventory.models.Tablet import Tablet
-from backend.apps.v1.inventory.models.Dimension import Dimension
-from backend.apps.v1.inventory.models.Size import Size
+
+from backend.apps.v1.inventory.TDGs.DesktopTDG import DesktopTDG
+
 
 
 class ItemView(APIView):
 
     authentication_classes = ()
     permission_classes = ()
+
+    def get(self, request):
+        self.desktop = Desktop(
+            'ZZZZZZd',
+            'Razer Desktop',
+            46,
+            15.0,
+            'LBS',
+            2299.99,
+            'CAD',
+            'RAZER',
+            16,
+            'GB',
+            'INTEL',
+            4,
+            2,
+            'TB',
+            15,
+            30,
+            1,
+            'INCH'
+        )
+        DesktopTDG.insert(self.desktop)
+        result = DesktopTDG.find(self.desktop.modelNumber)
+        print(result)
+
+        return Response()
 
     def post(self, request):
         store = Store()
@@ -24,37 +52,32 @@ class ItemView(APIView):
         print(itemData)
         try:
             if itemType == "Desktop":
-                dimension = Dimension(itemData["dx"], itemData["dy"], itemData["dz"])
                 item = Desktop(
                     itemData["modelNumber"], itemData["name"], itemData["quantity"],
                     itemData["weight"], itemData["weightFormat"], itemData["price"], itemData["priceFormat"], itemData["brandName"],
                     itemData["ramSize"], itemData["ramFormat"], itemData["processorType"], itemData["numCores"],
-                    itemData["hardDriveSize"], itemData["hardDriveFormat"], dimension
+                    itemData["hardDriveSize"], itemData["hardDriveFormat"], itemData["dx"], itemData["dy"], itemData["dz"]
                 )
             elif itemType == "Monitor Display":
-                size = Size(itemData["size"])
                 item = MonitorDisplay(
                     itemData["modelNumber"], itemData["name"], itemData["quantity"],
-                    itemData["weight"], itemData["weightFormat"], itemData["price"], itemData["priceFormat"], itemData["brandName"], size
+                    itemData["weight"], itemData["weightFormat"], itemData["price"], itemData["priceFormat"], itemData["brandName"], itemData["size"]
                 )
             elif itemType == "Laptop":
-                size = Size(itemData["size"])
                 item = Laptop(
                     itemData["modelNumber"], itemData["name"], itemData["quantity"],
                     itemData["weight"], itemData["weightFormat"], itemData["price"], itemData["priceFormat"], itemData["brandName"],
                     itemData["ramSize"], itemData["ramFormat"], itemData["processorType"], itemData["numCores"],
                     itemData["hardDriveSize"], itemData["hardDriveFormat"],
-                    itemData["containCamera"], itemData["isTouch"], itemData["batteryInfo"], itemData["os"], size
+                    itemData["containCamera"], itemData["isTouch"], itemData["batteryInfo"], itemData["os"], itemData["size"]
                 )
             elif itemType == "Tablet":
-                dimension = Dimension(itemData["dx"], itemData["dy"], itemData["dz"])
-                size = Size(itemData["size"])
                 item = Tablet(
                     itemData["modelNumber"], itemData["name"], itemData["quantity"],
                     itemData["weight"], itemData["weightFormat"], itemData["price"], itemData["priceFormat"], itemData["brandName"],
                     itemData["ramSize"], itemData["ramFormat"], itemData["processorType"], itemData["numCores"],
-                    itemData["hardDriveSize"], itemData["hardDriveFormat"], itemData["os"], dimension,
-                    size, itemData["cameraInfo"], itemData["batteryInfo"]
+                    itemData["hardDriveSize"], itemData["hardDriveFormat"], itemData["os"], itemData["dx"], itemData["dy"], itemData["dz"],
+                    itemData["size"], itemData["cameraInfo"], itemData["batteryInfo"]
                 )
         except Exception as error:
             print(error)
