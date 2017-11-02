8c2204ae3841d3686c4386e4481c68d7c75b4860
Frederic Daigle <Freddydaigle@gmail.com>
2017-11-01 20:38:22 -0400

    commented out part of Inventory class that will be refactored into mappers, left it for reference

--- backend/apps/v1/inventory/models/Inventory.py
+++ backend/apps/v1/inventory/models/Inventory.py
@@ -110,168 +110,151 @@ class Inventory(object):
 
     def requestInventoryList(self):
 
-        queryTablet = """
-            SELECT * FROM item, tablet
-            WHERE UPPER(item.type) = "TABLET"
-            AND item.modelNumber = tablet.modelNumber;
-        """
-
-        queryDesktop = """
-            SELECT * from item, desktop
-            WHERE UPPER(item.type) = "DESKTOP"
-            AND item.modelNumber = desktop.modelNumber;
-        """
-
-        queryMonitor = """
-            SELECT * from item, monitorDisplay
-            WHERE UPPER(item.type) = "MONITOR"
-            AND item.modelNumber = monitorDisplay.modelNumber;
-        """
-
-        queryLaptop = """
-            SELECT * from item, laptop
-            WHERE UPPER(item.type) = "LAPTOP"
-            AND item.modelNumber = laptop.modelNumber;
-        """
-
-        result = list()
-
-        with Database() as cursor:
-
-            try:
-
-                cursor.execute(queryTablet)
-                for row in cursor.fetchall():
-                    result.append(
-                        Tablet(
-                            row.get('modelNumber'),
-                            row.get('name'),
-                            row.get('quantity'),
-                            row.get('weight'),
-                            row.get('weightFormat'),
-                            row.get('price'),
-                            row.get('priceFormat'),
-                            row.get('brandName'),
-                            row.get('ramSize'),
-                            row.get('ramFormat'),
-                            row.get('processorType'),
-                            row.get('numCores'),
-                            row.get('hardDriveSize'),
-                            row.get('hardDriveFormat'),
-                            row.get('os'),
-                            Dimension(
-                                row.get('dx'),
-                                row.get('dy'),
-                                row.get('dz'),
-                                row.get('dimensionFormat'),
-                            ),
-                            Size(
-                                row.get('size'),
-                                row.get('sizeFormat')
-                            ),
-                            row.get('cameraInfo'),
-                            row.get('batteryInfo')
-                        )
-                    )
-
-                cursor.execute(queryDesktop)
-                for row in cursor.fetchall():
-                    result.append(
-                        Desktop(
-                            row.get('modelNumber'),
-                            row.get('name'),
-                            row.get('quantity'),
-                            row.get('weight'),
-                            row.get('weightFormat'),
-                            row.get('price'),
-                            row.get('priceFormat'),
-                            row.get('brandName'),
-                            row.get('ramSize'),
-                            row.get('ramFormat'),
-                            row.get('processorType'),
-                            row.get('numCores'),
-                            row.get('hardDriveSize'),
-                            row.get('hardDriveFormat'),
-                            Dimension(
-                                row.get('dx'),
-                                row.get('dy'),
-                                row.get('dz'),
-                                row.get('dimensionFormat'),
-                            ),
-                        )
-<<<<<<< HEAD
-                    )
-
-                cursor.execute(queryMonitor)
-                for row in cursor.fetchall():
-                    result.append(
-                        MonitorDisplay(
-                            row.get('modelNumber'),
-                            row.get('name'),
-                            row.get('quantity'),
-                            row.get('weight'),
-                            row.get('weightFormat'),
-                            row.get('price'),
-                            row.get('priceFormat'),
-                            row.get('brandName'),
-=======
-                    )
-
-                cursor.execute(queryMonitor)
-                for row in cursor.fetchall():
-                    result.append(
-                        MonitorDisplay(
-                            row.get('modelNumber'),
-                            row.get('name'),
-                            row.get('quantity'),
-                            row.get('weight'),
-                            row.get('weightFormat'),
-                            row.get('price'),
-                            row.get('priceFormat'),
-                            row.get('brandName'),
->>>>>>> e417c198f763d76e6c75a90301d753530d40c72a
-                            Size(
-                                row.get('size'),
-                                row.get('sizeFormat')
-                            )
-                        )
-                    )
-
-                cursor.execute(queryLaptop)
-                for row in cursor.fetchall():
-                    result.append(
-                        Laptop(
-                            row.get('modelNumber'),
-                            row.get('name'),
-                            row.get('quantity'),
-                            row.get('weight'),
-                            row.get('weightFormat'),
-                            row.get('price'),
-                            row.get('priceFormat'),
-                            row.get('brandName'),
-                            row.get('ramSize'),
-                            row.get('ramFormat'),
-                            row.get('processorType'),
-                            row.get('numCores'),
-                            row.get('hardDriveSize'),
-                            row.get('hardDriveFormat'),
-                            row.get('containsCamera'),
-                            row.get('isTouch'),
-                            row.get('batteryInfo'),
-                            row.get('os'),
-                            Size(
-                                row.get('size'),
-                                row.get('sizeFormat')
-                            )
-                        )
-                    )
-
-
-            except Exception as error:
-                print("Failed to retrieve Inventory list")
-                print(error)
-
-            print("{} UNIQUE items found".format(len(result)))
+        # queryTablet = """
+        #     SELECT * FROM item, tablet
+        #     WHERE UPPER(item.type) = "TABLET"
+        #     AND item.modelNumber = tablet.modelNumber;
+        # """
+        #
+        # queryDesktop = """
+        #     SELECT * from item, desktop
+        #     WHERE UPPER(item.type) = "DESKTOP"
+        #     AND item.modelNumber = desktop.modelNumber;
+        # """
+        #
+        # queryMonitor = """
+        #     SELECT * from item, monitorDisplay
+        #     WHERE UPPER(item.type) = "MONITOR"
+        #     AND item.modelNumber = monitorDisplay.modelNumber;
+        # """
+        #
+        # queryLaptop = """
+        #     SELECT * from item, laptop
+        #     WHERE UPPER(item.type) = "LAPTOP"
+        #     AND item.modelNumber = laptop.modelNumber;
+        # """
+        #
+        # result = list()
+        #
+        # with Database() as cursor:
+        #
+        #     try:
+        #
+        #         cursor.execute(queryTablet)
+        #         for row in cursor.fetchall():
+        #             result.append(
+        #                 Tablet(
+        #                     row.get('modelNumber'),
+        #                     row.get('name'),
+        #                     row.get('quantity'),
+        #                     row.get('weight'),
+        #                     row.get('weightFormat'),
+        #                     row.get('price'),
+        #                     row.get('priceFormat'),
+        #                     row.get('brandName'),
+        #                     row.get('ramSize'),
+        #                     row.get('ramFormat'),
+        #                     row.get('processorType'),
+        #                     row.get('numCores'),
+        #                     row.get('hardDriveSize'),
+        #                     row.get('hardDriveFormat'),
+        #                     row.get('os'),
+        #                     Dimension(
+        #                         row.get('dx'),
+        #                         row.get('dy'),
+        #                         row.get('dz'),
+        #                         row.get('dimensionFormat'),
+        #                     ),
+        #                     Size(
+        #                         row.get('size'),
+        #                         row.get('sizeFormat')
+        #                     ),
+        #                     row.get('cameraInfo'),
+        #                     row.get('batteryInfo')
+        #                 )
+        #             )
+        #
+        #         cursor.execute(queryDesktop)
+        #         for row in cursor.fetchall():
+        #             result.append(
+        #                 Desktop(
+        #                     row.get('modelNumber'),
+        #                     row.get('name'),
+        #                     row.get('quantity'),
+        #                     row.get('weight'),
+        #                     row.get('weightFormat'),
+        #                     row.get('price'),
+        #                     row.get('priceFormat'),
+        #                     row.get('brandName'),
+        #                     row.get('ramSize'),
+        #                     row.get('ramFormat'),
+        #                     row.get('processorType'),
+        #                     row.get('numCores'),
+        #                     row.get('hardDriveSize'),
+        #                     row.get('hardDriveFormat'),
+        #                     Dimension(
+        #                         row.get('dx'),
+        #                         row.get('dy'),
+        #                         row.get('dz'),
+        #                         row.get('dimensionFormat'),
+        #                     ),
+        #                 )
+        #             )
+        #
+        #         cursor.execute(queryMonitor)
+        #         for row in cursor.fetchall():
+        #             result.append(
+        #                 MonitorDisplay(
+        #                     row.get('modelNumber'),
+        #                     row.get('name'),
+        #                     row.get('quantity'),
+        #                     row.get('weight'),
+        #                     row.get('weightFormat'),
+        #                     row.get('price'),
+        #                     row.get('priceFormat'),
+        #                     row.get('brandName'),
+        #                     Size(
+        #                         row.get('size'),
+        #                         row.get('sizeFormat')
+        #                     )
+        #                 )
+        #             )
+        #
+        #         cursor.execute(queryLaptop)
+        #         for row in cursor.fetchall():
+        #             result.append(
+        #                 Laptop(
+        #                     row.get('modelNumber'),
+        #                     row.get('name'),
+        #                     row.get('quantity'),
+        #                     row.get('weight'),
+        #                     row.get('weightFormat'),
+        #                     row.get('price'),
+        #                     row.get('priceFormat'),
+        #                     row.get('brandName'),
+        #                     row.get('ramSize'),
+        #                     row.get('ramFormat'),
+        #                     row.get('processorType'),
+        #                     row.get('numCores'),
+        #                     row.get('hardDriveSize'),
+        #                     row.get('hardDriveFormat'),
+        #                     row.get('containsCamera'),
+        #                     row.get('isTouch'),
+        #                     row.get('batteryInfo'),
+        #                     row.get('os'),
+        #                     Size(
+        #                         row.get('size'),
+        #                         row.get('sizeFormat')
+        #                     )
+        #                 )
+        #             )
+        #
+        #
+        #     except Exception as error:
+        #         print("Failed to retrieve Inventory list")
+        #         print(error)
+        #
+        #     print("{} UNIQUE items found".format(len(result)))
 
         return result
 
