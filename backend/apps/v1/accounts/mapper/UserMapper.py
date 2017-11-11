71f320863958d1519fd80f73612998ff3d8f212f
kammy <kammy609@gmail.com>
2017-11-10 19:16:27 -0500

    updated authentication

--- backend/apps/v1/accounts/mapper/UserMapper.py
+++ backend/apps/v1/accounts/mapper/UserMapper.py
@@ -1,5 +1,5 @@
 from backend.apps.v1.accounts.models.Customer import Customer
-from backend.apps.v1.accounts.models.Client import Client
+from backend.apps.v1.accounts.models.Administrator import Administrator
 from backend.apps.v1.accounts.TDG.UserTDG import UserTDG
 
 class UserMapper:
@@ -32,11 +32,52 @@ class UserMapper:
             return True
 
     @staticmethod
-    def find(email):
+    def displayAllCustomer():
+
+        resultSet = UserTDG.findUsers()
+        
+        if(not resultSet):
+            print("found nothing")
+        else:
+            for row in resultSet:
+                print("User id: " + str(row['id']))
+                print("Email: " + row['email'])
+                print("Username: " + row['username'])
+                print("Firstname: " + row['firstname'])
+                print("Lastname: " + row['lastname'])
+                print("Address: " + row['address'])
+                print("Phone: " + row['phone'])
+                print("is logged: " + str(row['isLoggedIn']))
+                print("")
+
+    @staticmethod
+    def findCustomer(email):
 
         resultSet = UserTDG.findUser(email)
 
-        user = Client("","","",0,0,"")
+        user = Customer(0,"","",0,0,"","","","","","")
+        
+        for row in resultSet:
+            user.id = row['id']
+            user.email = row['email']
+            user.password = row['password']
+            user.isAdmin = 0
+            user.isLoggedIn = row['isLoggedIn']
+            user.timeStamp = row['timeStamp']
+            user.username = row['username']
+            user.firstname = row['firstname']
+            user.lastname = row['lastname']
+            user.address = row['address']
+            user.phone = row['phone']
+
+        return user
+
+    @staticmethod
+    def findAdmin(email):
+
+        resultSet = UserTDG.findUser(email)
+
+        user = Administrator(0,"","",0,0,"","","","","","")
         
         for row in resultSet:
             user.id = row['id']
@@ -45,21 +86,43 @@ class UserMapper:
             user.isAdmin = row['isAdmin']
             user.isLoggedIn = row['isLoggedIn']
             user.timeStamp = row['timeStamp']
+            user.username = row['username']
+            user.firstname = row['firstname']
+            user.lastname = row['lastname']
+            user.address = row['address']
+            user.phone = row['phone']
 
         return user
 
     @staticmethod
-    def update(client):
+    def update(user):
 
         try:
-            UserTDG.update(client)
+            UserTDG.update(user)
         except Exception as error:
             print(error)
 
     @staticmethod
-    def isLogged(email):
+    def delete(user):
 
-        user = UserMapper.find(email)
+        try:
+            UserTDG.delete(user.id)
+        except Exception as error:
+            print(error)
+
+    @staticmethod
+    def isLoggedCustomer(email):
+
+        user = UserMapper.findCustomer(email)
+        if(user.isLoggedIn is 1):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def isLoggedAdmin(email):
+
+        user = UserMapper.findAdmin(email)
         if(user.isLoggedIn is 1):
             return True
         else:
