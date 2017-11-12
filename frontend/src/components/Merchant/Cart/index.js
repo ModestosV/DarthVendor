8b699f99d4d39b205ee7892f8b9e48bea37b18f8
Roy Khoury <roykhoury@Roys-MacBook-Pro.local>
2017-11-12 17:26:22 -0500

    Added mock ups for shopping cart, add to cart/ remove from cart and checkout

--- frontend/src/components/Merchant/Cart/index.js
+++ frontend/src/components/Merchant/Cart/index.js
@@ -7,82 +7,143 @@ import Navigation from '../Navigation';
 
 class Cart extends Component {    
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            items:[{'modelNumber':'ZZZZZZZ',
-            'quantity':46,
-            'name':'Razer Desktop',
-            'weight':15.0,
-            'weightFormat':'lbs',
-            'price':2299.99,
-            'priceFormat':'CAD',
-            'brandName':'RAZER',
-            'type':'Desktop',
-            'ramSize':16,
-            'ramFormat':'GB',
-            'processorType':'INTEL',
-            'numCores':4,
-            'hardDriveSize':2,
-            'hardDriveFormat':'TB',
-            'dx':15,
-            'dy':30,
-            'dz':1,
-            'dimensionFormat':'INCH'}]
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      items:[{'modelNumber':'ZZZZZZZ',
+      'quantity':46,
+      'name':'Razer Desktop',
+      'weight':15.0,
+      'weightFormat':'lbs',
+      'price':2299.99,
+      'priceFormat':'CAD',
+      'brandName':'RAZER',
+      'type':'Desktop',
+      'ramSize':16,
+      'ramFormat':'GB',
+      'processorType':'INTEL',
+      'numCores':4,
+      'hardDriveSize':2,
+      'hardDriveFormat':'TB',
+      'dx':15,
+      'dy':30,
+      'dz':1,
+      'dimensionFormat':'INCH'}],
+      errorMsg: null
     }
+  }
 
-    componentWillMount() {  
-        const {history} = this.props;      
-        console.log(localStorage);
-        
-        if (!localStorage.activeUser) {
-            history.push('/login');
-        }
+  componentWillMount() {
+    const {history} = this.props;
+    console.log(localStorage);
+    if (!localStorage.activeUser) {
+        history.push('/login');
     }
+  }
 
-    componentDidMount() {
-        //this.getCatalog();
-    }
-    
-    render() {
-        return (
-            <div>
-                <Navigation />
-                <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8 offset-md-2">
-                        <div className="mt-2"> <h1 className="m-0"> Shopping Cart </h1> </div>
-                        <div className="card mt-2">
-                          <ul className="list-group list-group-flush">
-                            <li className="list-group-item">
-                                <div className="row"> 
-                                    <div className="col-sm-12">
-                                        <div> Item 1 </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="list-group-item">
-                                <div className="row"> 
-                                    <div className="col-sm-12">
-                                        <div> Item 1 </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li className="list-group-item">
-                                <div className="row"> 
-                                    <div className="col-sm-12">
-                                        <div> Item 1 </div>
-                                    </div>
-                                </div>
-                            </li>
-                          </ul>
+  componentDidMount() {
+      this.cartItemsList();
+  }
+
+  cartItemsList() {
+ //  return axios({
+ //      params: {
+ //          userID: localStorage.activeUser.id
+ //      },
+ //      method:'get',
+ //      url: `${settings.API_ROOT}/cart`,   // needs to be change to correct URL
+ //      withCredentials: true
+ //  })
+ //  .then(results => {
+ //      const errorMsg = null;
+ //      const items = results.data.map(item => item);
+ //      this.setState({items});
+ //      this.setState({errorMsg});
+ //      console.log(items);
+ //  })
+ //  .catch(error => {
+ //   console.log(error);
+ //   const errorMsg = "Oops, something went wrong while fetching items from shopping cart!";
+ //   this.setState({errorMsg});
+ // })
+  }
+  handleDeleteCartItem(row) {
+    console.log("unlock item");
+    const data = row;
+
+    axios({
+      method: 'post',
+      url: `${settings.API_ROOT}/cart`, // need to change this
+      data: data,
+      headers: {
+          Authorization: "Token " + JSON.parse(localStorage.activeUser).token
+      }
+    })
+    .then(response => {
+      swal({
+        text: "Item deleted from cart!",
+        icon: "success",
+        button: "Ok",
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      swal({
+        title: "Woops!",
+        text: "Something went wrong!",
+        icon: "error",
+        button: "Ok",
+      });
+    })
+  }
+
+  handleCheckout() {
+    console.log("checking out");
+    console.log("unlock cart item");
+  }
+  render() {
+    const self = this;
+
+    return (
+      <div>
+        <Navigation />
+        <div className="row">
+          <div className="col-md-2"></div>
+            <div className="col-md-8 offset-md-2">
+              <div className="mt-2">
+                <h1 className="m-0"> Shopping Cart </h1> </div>
+                { !!this.state.errorMsg && <div className="fa fa-warning errorMsg"> {this.state.errorMsg} </div> }
+                <div className="card mt-2">
+                  <ul className="list-group list-group-flush">
+                    {self.state.items.map( (item) =>
+                      <li key={item.name}
+                        className="list-group-item">
+                        <div className="row">
+                          <div className="col-sm-11">
+                            <div>{item.name}</div>
+                          </div>
+                          <div className="col-sm-1 text-right">
+                            <i className="fa fa-times"
+                              aria-hidden="true"
+                              onClick={() => this.handleDeleteCartItem(item)}>
+                            </i>
+                          </div>
                         </div>
-                    </div>
+                      </li>
+                    )}
+                  </ul>
+                </div>
+                <div className="justify-content-md-center">
+                  <button className="btn btn-success float-right col-sm-2 mt-2"
+                    onClick={() => this.handleCheckout()}>
+                    Checkout
+                  </button>
                 </div>
             </div>
-        )
-    }
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Cart;
