0d5e58c75cf0cc49245a128c7d9ca99383425c47
Frederic Daigle <Freddydaigle@gmail.com>
2017-11-12 18:50:25 -0500

    implemented deleteItemID endpoint

--- backend/apps/v1/inventory/urls.py
+++ backend/apps/v1/inventory/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import url
 from .views.inventory import InventoryView
 from .views.item import AddItemSpecView, InitiateEdit, TerminateEdit, ModifyItemSpecView, getEditStateView, AddQuantityView
-from .views.itemID import ItemIDsForSpecView
+from .views.itemID import ItemIDsForSpecView, DeleteItemID
 
 
 urlpatterns = [
@@ -13,5 +13,6 @@ urlpatterns = [
     url(r'^initiateEdit', InitiateEdit.as_view()),
     url(r'^terminateEdit', TerminateEdit.as_view()),
     url(r'^getEditState', getEditStateView.as_view()),
-    url(r'^getItemIDs', ItemIDsForSpecView.as_view())
+    url(r'^getItemIDs', ItemIDsForSpecView.as_view()),
+    url(r'^deleteItemID', DeleteItemID.as_view())
 ]
