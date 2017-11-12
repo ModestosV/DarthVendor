d272a0ca5bbadc44e3165892634da31275326239
kemoua <kemoua@gmail.com>
2017-11-11 20:56:25 -0500

    merge + removed columns

--- frontend/src/components/Admin/Home/index.js
+++ frontend/src/components/Admin/Home/index.js
@@ -16,24 +16,6 @@ class Home extends Component {
         };
     }
 
-    componentWillMount() {
-        console.log(localStorage);
-        
-        const {dispatch, history} = this.props;
-        
-        // Redirect if user is not logged in
-        if (!localStorage.activeUser) {
-            history.push('/login');
-        } else {
-            const activeUser = JSON.parse(localStorage.activeUser);
-
-            if (activeUser.isAdmin === false) {
-                // Redirect to merchant home page                
-                history.push('merchant');
-            }            
-        }        
-    }
-
     componentDidMount() {
         this.itemsList();
     }
@@ -47,11 +29,9 @@ class Home extends Component {
         .then(results => {
             const errorMsg = null;
             const items = results.data.map(item => item);
-            const itemsList = items;
-            this.setState({itemsList});
             this.setState({items});
             this.setState({errorMsg});
-            console.log(this.state);
+            console.log(items);
         })
         .catch(error => {
          console.log(error);
@@ -60,18 +40,7 @@ class Home extends Component {
        })
     }
 
-    filterItems(e) {
-        if(e.target.value == ""){
-            this.setState({itemsList: this.state.items});
-        } else {
-            let filteredItems = this.state.items.filter(item => item.weight < e.target.value);
-            this.setState({itemsList: filteredItems});
-        }
-        
-    }
-
     render() {
-
         function cellFormat(cell, row){
             return '<i class="glyphicon glyphicon-usd"></i> ' + cell;
         }
@@ -92,16 +61,12 @@ class Home extends Component {
                     <h1> Inventory </h1>
                     { !!this.state.errorMsg && <div className="fa fa-warning errorMsg"> {this.state.errorMsg} </div> }
                     <br />
-                    <input type="text" onBlur={(e) => this.filterItems(e)}/>
-                    <BootstrapTable data={this.state.itemsList} striped condensed hover pagination search scrolling>
+                    <BootstrapTable data={this.state.items} striped condensed hover pagination search scrolling>
                         <TableHeaderColumn dataField="modelNumber" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Model Number</TableHeaderColumn>
                         <TableHeaderColumn dataField="brandName" isKey={true} dataAlign="center" dataSort={true} dataFormat={cellFormat}>Brand Name</TableHeaderColumn>
                         <TableHeaderColumn dataField="type" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Type</TableHeaderColumn>
-                        <TableHeaderColumn dataField="weight" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Weight</TableHeaderColumn>
-                        <TableHeaderColumn dataField="weightFormat" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Weight Format</TableHeaderColumn>
-                        <TableHeaderColumn dataField="price" dataAlign="center" dataSort={true} sortFunc={sortFunc} dataFormat={cellFormat}>Price</TableHeaderColumn>
-                        <TableHeaderColumn dataField="priceFormat" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Price Format</TableHeaderColumn>
-                        <TableHeaderColumn dataField="quantity" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Quantity</TableHeaderColumn>
+                        <TableHeaderColumn dataField="weight" dataAlign="center" dataSort={true} dataFormat={cellFormat}>Weight (lbs)</TableHeaderColumn>
+                        <TableHeaderColumn dataField="price" dataAlign="center" dataSort={true} sortFunc={sortFunc} dataFormat={cellFormat}>Price (CAD)</TableHeaderColumn>
                     </BootstrapTable>
                 </div>
             </div>
