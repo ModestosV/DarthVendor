f016f174e08381f7ec3fb307e82a8e2bea2ce45a
Frederic Daigle <Freddydaigle@gmail.com>
2017-11-12 16:49:08 -0500

    changed AUthentication

--- backend/apps/v1/accounts/views/logout.py
+++ backend/apps/v1/accounts/views/logout.py
@@ -3,25 +3,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from backend.utils.database import Database
 
+from backend.apps.v1.accounts.ObjectSession import ObjectSession
+
 
 class LogoutView(APIView):
 
     def get(self, request):
         """
-            Remove API token. 
+            Logout
         """
 
-        with Database() as cursor:
-            query = """ 
-                DELETE
-                FROM token
-                WHERE token = '{}'
-            """.format(request.META['HTTP_AUTHORIZATION'])            
+        ObjectSession.sessions[request.session['user']] = None
+        request.session['user'] = None
 
-            try:
-                cursor.execute(query)                    
-            except Exception as error: 
-                print(error) 
-                return Response({}, status=status.HTTP_400_BAD_REQUEST)
-            
         return Response({}, status=status.HTTP_200_OK)
