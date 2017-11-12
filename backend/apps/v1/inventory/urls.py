8a8adbb03e9ad6c11658c04a232ce8bbf45f0f03
Frederic Daigle <Freddydaigle@gmail.com>
2017-11-12 18:57:59 -0500

    implemented cancelEditing

--- backend/apps/v1/inventory/urls.py
+++ backend/apps/v1/inventory/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from .views.inventory import InventoryView
-from .views.item import AddItemSpecView, InitiateEdit, TerminateEdit, ModifyItemSpecView, getEditStateView, AddQuantityView
+from .views.item import AddItemSpecView, InitiateEdit, TerminateEdit, CancelEditView, ModifyItemSpecView, getEditStateView, AddQuantityView
 from .views.itemID import ItemIDsForSpecView, DeleteItemID
 
 
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^addQuantity', AddQuantityView.as_view()),
     url(r'^initiateEdit', InitiateEdit.as_view()),
     url(r'^terminateEdit', TerminateEdit.as_view()),
+    url(r'^cancelEdit', CancelEditView.as_view()),
     url(r'^getEditState', getEditStateView.as_view()),
     url(r'^getItemIDs', ItemIDsForSpecView.as_view()),
     url(r'^deleteItemID', DeleteItemID.as_view())
