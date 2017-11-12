713a8e54a20842ada5c861d86b21f1f0a1592031
kemoua <kemoua@gmail.com>
2017-11-12 18:30:32 -0500

    log buttons

--- frontend/src/components/Merchant/Catalog/index.js
+++ frontend/src/components/Merchant/Catalog/index.js
@@ -29,18 +29,6 @@ class Catalog extends Component {
     componentWillMount() {  
         const {history} = this.props;      
         console.log(localStorage);
-        
-        if (!localStorage.activeUser) {
-            history.push('/login');
-        } else {
-            const activeUser = JSON.parse(localStorage.activeUser);
-
-            // Making sure user does not have admin permission
-            if (activeUser.adminPermission === true) {
-                // Redirect to admin home page                
-                history.push('/admin/');
-            }            
-        } 
     }
 
     componentDidMount() {
